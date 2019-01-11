e18851f40260863d78738525ec9127db3cc5b2ad
Ворончихин Александр (Voronchihin_AV) <Voronchihin_AV@directum.ru>
2019-01-11 15:48:05 +0400

    Delete link to table sungero_system_entitytype

--- app/users.py
+++ app/users.py
@@ -4,13 +4,11 @@
 def get_logins_with_jobs_inprocess(dbconn, job_type, count_jobs):
     query = "SELECT l.loginname " \
             + "FROM sungero_wf_assignment a " \
-            + "INNER JOIN sungero_system_entitytype et " \
-            + "ON a.discriminator = et.typeguid " \
             + "INNER JOIN sungero_core_recipient r " \
             + "ON r.id = a.performer " \
             + "INNER JOIN sungero_core_login l " \
             + "ON r.login = l.id " \
-            + " WHERE a.Status = 'InProcess'	AND et.typeguid ='{0}' ".format(job_type) \
+            + " WHERE a.Status = 'InProcess' AND a.discriminator ='{0}' ".format(job_type) \
             + "GROUP BY l.loginname " \
             + "HAVING count(*) > {0} ".format(count_jobs) \
             + "ORDER BY count(*) desc"
