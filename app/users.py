b7acccb8725509d832ed9e42c4a6c9a265bb2303
Ворончихин Александр (Voronchihin_AV) <Voronchihin_AV@directum.ru>
2019-01-11 17:49:46 +0400

    Get users with unread simple notices

--- app/users.py
+++ app/users.py
@@ -19,3 +19,23 @@ def get_logins_with_jobs_inprocess(dbconn, job_type, count_jobs):
         result = cur.fetchall()
 
         return result
+
+
+def get_logins_with_unread_notices(dbconn, job_type, count_notices):
+    query = "SELECT l.loginname " \
+            + "FROM sungero_wf_assignment a " \
+            + "INNER JOIN sungero_core_recipient r " \
+            + "ON r.id = a.performer " \
+            + "INNER JOIN sungero_core_login l " \
+            + "ON r.login = l.id " \
+            + "WHERE a.discriminator ='{0}' ".format(job_type) \
+            + "GROUP BY l.loginname " \
+            + "HAVING count(*) > {0} ".format(count_notices) \
+            + "ORDER BY count(*) desc"
+
+    with dbconn.connection() as connection:
+        cur = connection.cursor()
+        cur.execute(query)
+        result = cur.fetchall()
+
+        return result
