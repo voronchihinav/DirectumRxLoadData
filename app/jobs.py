1a78170726445470cf8226a2c522948d51d470cc
KarlovAV <46926169+KarlovAV@users.noreply.github.com>
2019-01-22 18:16:29 +0400

    Update jobs.py
    Added a request to receive a task

--- app/jobs.py
+++ app/jobs.py
@@ -44,3 +44,27 @@ def get_all_jobs(dbconn, performer, discriminator, skip):
         cur.execute(query)
         result = cur.fetchall()
         return result
+
+    
+    
+def get_task_with_filter(dbconn, author, discriminator, filter=""):
+    if filter is None:
+        filter = ""
+    query = "SELECT "
+    if dbconn.engine == 'mssql':
+        query = query + " TOP 10 "
+    query = query + "t.id FROM sungero_wf_task t " \
+            + "WHERE t.author = {0} ".format(author) \
+            + "AND t.status = 'InProcess' " \
+            + "AND t.discriminator = '{0}' ".format(discriminator) \
+            + "AND t.subject like '%{0}%' ".format(filter) \
+            + "ORDER BY created desc "
+    if dbconn.engine == 'psql':
+        query = query \
+                + "LIMIT 10"
+
+    with dbconn.connection() as connection:
+        cur = connection.cursor()
+        cur.execute(query)
+        result = cur.fetchall()
+        return result
